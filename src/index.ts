22080ef3b80c7ca16979581c141b9098096a6e41
yuriel <yuriel3183@gmail.com>
2017-02-21 15:50:19 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import {FlipFlappers} from "./app/flip-flappers";
+/**
+ * Created by yuriel on 2/6/17.
+ */
+export class AppMain {
+    constructor(public flapper: FlipFlappers) {}
+
+    flapping() {
+        this.flapper.flaping();
+    }
+}
